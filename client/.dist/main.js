32eded4a5c17c8bc1d510dbb7c7e912c6bc8f180
Kaygisiz Benjamin <b.kaygisiz@live.fr>
2021-12-10 17:03:12 +0100

    start ignoring files

--- client/.dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _client = require("./client");
-
-var host = process.argv[2];
-var port = process.argv[3];
-console.log('arg : ' + host + port);
-(0, _client.connect)(host, port);
--- server/.dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _server = require("./server");
-
-var port = process.argv[2];
-(0, _server.launch)('localhost', port);
